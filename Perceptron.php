a09947420e02ff4424e791eeeb055332e9a1b0cf
Lucas Macedo <luuckymacedo@gmail.com>
2021-02-01 23:53:25 -0300

    Adicionado Multiplicação de Matrizes

--- Perceptron.php
+++ Perceptron.php
@@ -44,18 +44,30 @@ class Perceptron
 
             for ($i = 0; $i < count($X); $i++) {
 
+                $target = $y[$i];
+                $xi = $X[$i];
 
+                $update = $this->taxa_aprendizado * ($target - $this->predict($xi, $weight_));
+                dd($update);
             }
         }
     }
 
-    public function predict()
+    public function predict($X, $weight_)
     {
-
+        return $this->net_input($X, $weight_);
     }
 
-    public function net_input()
+
+    public function net_input($X, $weight_)
     {
+
+        $bias = $weight_[0];
+        unset($weight_[0]);
+        $weight_ = array_values($weight_);
+        $X = array_values($X);
+
+        return $this->multiply($X, $weight_) + $bias;
 
     }
 
@@ -64,14 +76,41 @@ class Perceptron
      * @param int $size
      * @return array
      */
-    public
-    function randoDecimal($size = 1)
+    public function randoDecimal($size = 1)
     {
         $data = [];
         for ($i = 0; $i <= $size; $i++) {
             $data[] = rand(0, 1) == 1 ? (rand(0, 100) - 100) / 100 : -(rand(0, 20) - 100) / 100;
         }
         return $data;
+    }
+
+    /**
+     * Map multiply against multiple arrays
+     *
+     * [x₁ * y₁, x₂ * y₂, ... ]
+     *
+     * @param array ...$arrays Two or more arrays of numbers
+     *
+     * @return array
+     *
+     */
+    public static function multiply(array ...$arrays)
+    {
+
+
+        $number_of_arrays = \count($arrays);
+        $length_of_arrays = \count($arrays[0]);
+
+        $products = \array_fill(0, $length_of_arrays, 1);
+
+        for ($i = 0; $i < $length_of_arrays; $i++) {
+            for ($j = 0; $j < $number_of_arrays; $j++) {
+                $products[$i] *= $arrays[$j][$i];
+            }
+        }
+
+        return array_sum($products);
     }
 
 }
--- index.php
+++ index.php
@@ -4,17 +4,17 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 include "vendor/autoload.php";
 include "Perceptron.php";
-$response = file('https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+
+$response = file('iris.data', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
 $header_values = ["sepal_lenght", "sepal_width", "petal_leght", "petal_width", "class"];
 
 $data = array_map(function ($item) use ($header_values) {
-    return array_combine($header_values, explode(',', $item));
+    return explode(',', $item);
 }, $response);
-
 
 $y = array_map(function ($item) {
     return $item == 'Iris-setosa' ? 1 : -1;
-}, array_column($data, 'class'));
+}, array_column($data, 4));
 
 // Remove a Coluna "Class" do DataSet
 array_walk($data, function (&$v) {
